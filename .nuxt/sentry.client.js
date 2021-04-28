b89e522c1ca6358249d6165b624b192999f27540
orangemn6 <jbensa081@gmail.com>
2021-04-27 21:21:49 -0400

    should work soon

--- .nuxt/sentry.client.js
+++ .nuxt/sentry.client.js
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/browser'
 import { Dedupe, ExtraErrorData, ReportingObserver, RewriteFrames, Vue } from '@sentry/integrations'
 
 export default function (ctx, inject) {
-  const opts = Object.assign({}, {"dsn":"https:\u002F\u002Fd6352c3a17124246a0effb6282e65e3e@o108936.ingest.sentry.io\u002F5269230","environment":"production"}, {
+  const opts = Object.assign({}, {"dsn":"https:\u002F\u002Fd6352c3a17124246a0effb6282e65e3e@o108936.ingest.sentry.io\u002F5269230","environment":"development"}, {
     integrations: [
       new Dedupe({}),
       new ExtraErrorData({}),
